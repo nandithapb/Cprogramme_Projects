61b511ba7fe31d1c6c2d84107c09859af9e69917
nandithapb <51952055+nandithapb@users.noreply.github.com>
2019-12-09 18:00:40 +0530

    Update Main.java

--- Add first and third digit/Main.java
+++ Add first and third digit/Main.java
@@ -1,11 +1,16 @@
+//header
 #include<stdio.h>
+//main function
 int main()
 {
+  //variable declarations
   int n,a,b;
+  //scanning function
   scanf("%d %d",&n);
   a=n/100;
   b=n%10;
   a=a+b;
+  //printing function
   printf("%d",a);
   return 0;
 }
